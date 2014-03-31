fa1cbf2b2ba14802e920d655b4c1ac8411aa0783
Mauricio Navarro Miranda <mautematico@mozilla-mexico.org>
2014-03-30 22:17:16 -0600

    Descripción de ommenlinea.php

--- ommexamen.php
+++ ommexamen.php
@@ -1,4 +1,12 @@
 <?php
+/*
+Plugin Name: OMMQro - Exámenes en línea
+Plugin URI: http://ommqro.mx
+Description: Genera y evalúa exámenes tomando problemas de una base de datos predefinida.
+Author: Mauricio Navarro Miranda
+Version: 0.1
+Author URI: http://blog.mautematico.com
+*/
 
 include_once dirname( __FILE__ ) . '/install.php';
 register_activation_hook( __FILE__, array( 'ommexamen_install' ) );
