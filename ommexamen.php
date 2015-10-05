ad45f52d34c4e5e13479720bf934fde134588d96
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2015-10-05 14:54:13 -0500

    Update ommexamen.php

--- ommexamen.php
+++ ommexamen.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: OMMQro - Exámenes en línea
-Plugin URI: http://ommqro.mx
+Plugin URI: https://ommqro.mx
 Description: Genera y evalúa exámenes tomando problemas de una base de datos predefinida.
 Author: Mauricio Navarro Miranda
 Version: 0.1
-Author URI: http://blog.mautematico.com
+Author URI: https://blog.mautematico.com
 */
 
 include_once dirname( __FILE__ ) . '/install.php';
